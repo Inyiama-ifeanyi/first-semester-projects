16bca8e69b01b56b0894926a119e2a87b4981379
Ifeanyichukwu Inyiama <inyiamaifeanyi@gmail.com>
2025-10-05 22:56:43 +0100

    Completed Month 1 & 2 projects

--- month2_minimart_analysis/python/utils.py
+++ month2_minimart_analysis/python/utils.py
@@ -5,6 +5,7 @@ Created on Sun Oct  5 11:31:38 2025
 @author: PC
 """
 
+# Filter large orders
 def flag_large_orders(orders, products):
     large_orders = []
     for order in orders:
@@ -14,5 +15,6 @@ def flag_large_orders(orders, products):
             large_orders.append(order)
     return large_orders
 
+# Convert price from NGN to USD
 def convert_prices(products, rate=0.000667):  # NGN to USD
     return {pid: {'name': p['product_name'], 'price': p['price'] * rate} for pid, p in products.items()}
